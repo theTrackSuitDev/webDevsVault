a9d9159075d823f5f9b61d1deddc3ec635427e00
theTrackSuitDev <dimiro89@gmail.com>
2024-07-13 20:51:36 +0300

    Created Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
   return (
     <>
       <div className="content">
+
+        <Header />
+
         
       </div>
     </>
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,44 @@
+import logo from "../../assets/images/light-bulb-gear-13626.png"
+
+export default function Header() {
+    return (
+        <header>
+            <div className="logo">
+                <a href="">
+                    {" "}
+                    <img src={logo} alt="Bulb-logo" />
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li id="nav-home">
+                        <a href="#" className="nav-active">
+                            Home
+                        </a>
+                    </li>
+                    <li id="nav-vault">
+                        <a href="#">Vault</a>
+                    </li>
+                    <li id="nav-add">
+                        <a href="#">Add Resource</a>
+                    </li>
+                    <li id="nav-profile">
+                        <a href="#">Profile</a>
+                    </li>
+                    <li id="nav-login">
+                        <a href="#">Login</a>
+                    </li>
+                    <li id="nav-register">
+                        <a href="#">Register</a>
+                    </li>
+                    <li id="nav-about">
+                        <a href="#">About</a>
+                    </li>
+                    <li id="nav-logout">
+                        <a href="#">Logout</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
