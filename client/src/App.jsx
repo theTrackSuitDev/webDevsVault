60e6d73d253a07184aff1eef08bc17eb24460de5
theTrackSuitDev <dimiro89@gmail.com>
2024-07-24 03:07:28 +0300

    Created static Edit component that shares folder and styles with the Add component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-
 import "./App.css";
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Catalog from "./components/catalog/Catalog";
-import AddResource from "./components/add-resource/AddResource";
+import AddResource from "./components/add-edit-resource/AddResource";
 import Profile from "./components/profile/Profile";
+import EditResource from "./components/add-edit-resource/EditResource";
 
 function App() {
     return (
@@ -21,6 +21,7 @@ function App() {
                         <Route path="/about" element={<About />}/>
                         <Route path="/vault" element={<Catalog />}/>
                         <Route path="/add-resource" element={<AddResource />}/>
+                        <Route path="/edit/*" element={<EditResource />}/>
                         <Route path="/profiles/*" element={<Profile />}/>
                     </Routes>
                 </main>
